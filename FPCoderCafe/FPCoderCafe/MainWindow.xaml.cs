7283a4ca8fa4fd8d2918e53691121e651802cc29
Minh Bui <bqminh1806@gmail.com>
2022-03-26 23:12:55 -0700

    Add app navigation structure

---
+++ FPCoderCafe/FPCoderCafe/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using FPCoderCafe.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FPCoderCafe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static MainWindow mainWindow;
+        public static MainWindow GetMainWindow()
+        {
+            return mainWindow;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainWindow = this;
+            if (string.IsNullOrEmpty(Properties.Settings.Default.ManagerPinCode))
+            {
+                MainFrame.Content = new ManagerUserControl();
+
+            }
+            else
+            {
+                MainFrame.Content = new ManagerUserControl();
+            }
+        }
+    }
+}
---
+++ FPCoderCafe/FPCoderCafe/UserControls/ControlPanelUserControl.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static FPCoderCafe.Properties.Settings;
+
+namespace FPCoderCafe.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ConfigUserControl.xaml
+    /// </summary>
+    public partial class ControlPanelUserControl : UserControl
+    {
+        public ControlPanelUserControl()
+        {
+            InitializeComponent();
+            LoadSettings();
+            ToggleEventHandlers(true);
+        }
+
+        private void LoadSettings()
+        {
+            ManagerPinCode.Text = Default.ManagerPinCode;
+        }
+
+        public void ToggleEventHandlers(bool toggle)
+        {
+            if (toggle)
+            {
+                SaveButton.Click += SaveSettingEventHandler;
+                SwitchModeButton.Click += SwitchModeEventHandler;
+            }
+            else
+            {
+                SaveButton.Click -= SaveSettingEventHandler;
+                SwitchModeButton.Click -= SwitchModeEventHandler;
+            }
+        }
+
+        public void SaveSettingEventHandler(object o, EventArgs args) 
+        {
+            //Validate input
+            string ErrorMessage = string.Empty;
+            var pinCode = ManagerPinCode.Text;
+            if (string.IsNullOrWhiteSpace(pinCode))
+            {
+                ErrorMessage += "Pincode is invalid\n";
+            }
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                MessageBox.Show(ErrorMessage,"Could not save settings");
+                return;
+            }
+            //Save settings to app config
+            Default.ManagerPinCode = pinCode;
+            Default.Save();
+        }
+
+        public void SwitchModeEventHandler(object o, EventArgs args)
+        {
+            MainWindow.GetMainWindow().MainFrame.Content = new FoodMenuUserControl();
+        }
+    }
+}
