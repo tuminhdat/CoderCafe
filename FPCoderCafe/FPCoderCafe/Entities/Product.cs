589057f293e37c2dc84ca35b98c90d4c7a17420c
Minh Bui <bqminh1806@gmail.com>
2022-04-04 13:09:58 -0700

    Fix image path property spelling

--- FPCoderCafe/FPCoderCafe/Entities/Product.cs
+++ FPCoderCafe/FPCoderCafe/Entities/Product.cs
@@ -18,7 +18,7 @@ namespace FPCoderCafe.Entities
         public Category Category { get; set; }
         public string ImageName { get; set; }
         [NotMapped]
-        public string FullImagePath { get => "./image/" + ImageName; }
+        public string FullImagePath { get => "./Images/" + ImageName; }
         public Product()
         {
 
